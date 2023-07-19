061fb2cffb47021fb33231e0bc53049da49e6a16
Abubakar Shaikh <shaikhabubakar2380@gmail.com>
2023-07-19 23:31:54 +0530

    server config updated

--- server/index.js
+++ server/index.js
@@ -10,7 +10,13 @@ import postRouter from "./routes/post-routes.js";
 //Initialization
 const app = express();
 dotenv.config();
-app.use(cors())
+app.use(cors(
+  {
+    origin:[ process.env.FRONTEND_ORIGIN_URL ],
+    methods: ["GET", "POST", "PUT", "DELETE"],
+    credentials: true
+  }
+))
 app.use(express.json({ limit: '10mb' }));
 
 // Middleware to parse JSON data
