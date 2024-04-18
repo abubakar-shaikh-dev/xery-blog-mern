942312ce4756de48d5b62a7961d086bf9364cfdd
Abubakar Shaikh (XERY) <64248752+abubakar-shaikh-dev@users.noreply.github.com>
2024-04-18 11:11:19 +0530

    Update index.js

--- server/index.js
+++ server/index.js
@@ -12,7 +12,7 @@ const app = express();
 dotenv.config();
 app.use(cors(
   {
-    origin:[ process.env.FRONTEND_ORIGIN_URL ],
+    origin:*,
     methods: ["GET", "POST", "PUT", "DELETE"],
     credentials: true
   }
